c5a43e7d9965ef9bface694f3409901188a05545
Caleb Stevenson <cgrahamstevenson@gmail.com>
2017-04-05 22:11:48 -0700

    add twig templates for home and two pages

--- app/app.php
+++ app/app.php
@@ -3,41 +3,16 @@
     require_once __DIR__.'/../vendor/autoload.php';
 
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    		'twig.path' => __DIR__.'/../views'
+    	));
 
-    $app->get("/", function() {
-		return "
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
-			<title>Home Page</title>
-		</head>
-		<body>
-			<h1>HOME</h1>
-			<h1><a href='/two'>TWO</a></h1>
-		</body>
-		</html>
-		"
-		;
+    $app->get("/", function() use ($app) {
+		return $app['twig']->render('home.html.twig');
     });
 
-    //Server cannot find this URL. 
-    //Tried updating the autoload file.
-    $app->get("/two", function() {
-    	return "
-    	<!DOCTYPE html>
-    	<html>
-    	<head>
-    		<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
-    		<title>TWO</title>
-    	</head>
-    	<body>
-    		<h1>TWO</h1>
-    		<h1><a href='/'>HOME</a></h1>
-    	</body>
-    	</html>
-    	"
-    	;
+    $app->get("/two", function() use ($app) {
+    	return $app['twig']->render('two.html.twig');
     });
 
     return $app;
